4eab0c88dc0298adae0de0349701b0abe9912a63
Nikita Dhawan <nikita@cs.toronto.edu>
2025-02-15 21:17:36 -0500

    curated -> extractions untested

--- estimate_ate.py
+++ estimate_ate.py
@@ -8,82 +8,124 @@ import hydra
 from omegaconf import DictConfig
 from hydra.utils import instantiate
 import nest_asyncio
-    
-def save_path_fn(cfg, final=False):
-    save_path = cfg.save_path + cfg.data.dataset + "/"
-    if final:
-        save_path += cfg.date_time + "_"
-    save_path += cfg.prompt_type + "_" + cfg.data.dataset + "_" + cfg.experiment_name + "_" + cfg.model.llm_name 
-    save_path += "_samples.csv"
-    return save_path
 
-def get_model(cfg):
-    prompt_file = open(cfg.prompt_dir + cfg.data.dataset + "_" + cfg.prompt_type + ".txt", "r")
-    system_template = prompt_file.read()
-    human_template = "\n## Input \n> {post}"
+from naturalv2.eval.experiment import Experiment
+from naturalv2.utils import *
 
-    key_file = open(cfg.model.key_path, "r") 
-    llm_key = key_file.read().rstrip('\n')
-                
-    llm = direct_gpt(model_name=cfg.model.llm_name, 
-            openai_api_key=llm_key,
-            system_template=system_template,
-            human_template=human_template,
-            temperature=cfg.model.temperature,
-            max_tokens=None,
-            seed=cfg.seed,
-            response_format={"type": "json_object"}
-            )
+def extract_covariates(input_df, experiment, model, save_path, impute=False):
+    model.system_template = "" # TODO
+    model.human_template = "\n## Input \n>{report}"
+    structured_columns = experiment.covariate_names + experiment.treatment_names + experiment.outcome_names + "report"
     
-    return llm
+    llm_samples_df = pd.DataFrame(columns=structured_columns)
+    llm_inputs = []
+    for i, row in tqdm(input_df.iterrows()):
+        report = row["report"]
+        llm_inputs.append({"report": report})
+        if len(llm_inputs) >= model.batch_size or len(llm_inputs) == len(input_df):
+            llm_out_dicts = llm.get_outputs(llm_inputs)
+            llm_out_dicts = [json.loads(text) for text in llm_out_dicts]
+            dict_to_save = [{
+                **llm_out_dicts[j], **{'report': llm_inputs[j]["report"]}
+            } for j in range(len(llm_inputs))]
+            df_to_save = pd.DataFrame.from_dict(dict_to_save)
+            llm_samples_df = pd.concat([llm_samples_df, df_to_save], ignore_index=True)
+            if impute: # TODO: Remove to use only new extractions - shouldn't change results much. 
+                input_df.update(llm_samples_df, overwrite=False)
+                llm_samples_df = input_df.copy().iloc[:len(llm_samples_df)]
+                llm_samples_df = dataset.discretize(llm_samples_df, hard_filter=False, inf=False)
+            llm_samples_df.to_csv(save_path) 
+            llm_inputs = []
+    
+    llm_samples_df.to_csv(save_path)
+    return llm_samples_df
+
+def filter_by_inclusion(samples_df, experiment):
+    samples_df = experiment.discretize(samples_df, hard_filter=True, inf=False)
+    samples_df = samples_df.map(lambda x: np.nan if x in ["Unknown", "unknown"] else x)
+    return samples_df
+
+def extract_conditionals(input_df, experiment, model, save_path, inclusion=False):
+    model.system_template = "" # TODO
+    llm_probs_df = pd.DataFrame()
+    to_enum = ["inclusion"] if inclusion else experiment.treatment_names + experiment.outcome_names
+    options = enumerate_strings(experiment.get_options(to_enum))
+    interleaved_options = qa_interleaved_enum(
+        experiment.get_question_prompt(to_enum),
+        experiment.get_options(to_enum),
+        options,
+        to_enum
+    )
+    idx_to_feat = enum_to_dcts(options, to_enum)
+    idx_to_feat = [experiment.transform_samples(dct) for dct in idx_to_feat]
+    llm_inputs, rows = [], []
+
+    for i, row in tqdm(input_df.iterrows()):
+        X = row["report"]
+        if not inclusion:
+            # get corresponding row from input_df
+            sample_row = input_df.loc[input_df["report"] == X]
+            if len(sample_row) == 0:
+                continue;
+            sample_row = sample_row[experiment.covariate_names]
+            sample_row = sample_row.to_dict('records')[0]
+            sample_text = get_sample_text(sample_row, experiment)
+            X += sample_text
+
+        llm_inputs.append(X)
+        cols = experiment.covariate_names + experiment.treatment_names + experiment.outcome_names + "report"
+        rows.append(row[cols])
+        if len(llm_inputs) >= model.batch_size:
+            post_probs, sample_indices = llm.compute_input_probs(llm_inputs, interleaved_options)
+            dict_to_save = [{**rows[j].to_dict(), 
+                             **idx_to_feat[sample_indices[j]], 
+                             **{"probs": post_probs[j]}} for j in range(len(llm_inputs))]
+            df_to_save = pd.DataFrame.from_dict(dict_to_save)
+            llm_probs_df = pd.concat([llm_probs_df, df_to_save], ignore_index=True)
+            llm_probs_df.to_csv(save_path)
+            llm_inputs, rows = [], []
+    
+    llm_probs_df.to_csv(save_path)
+    return llm_probs_df
+
         
 @hydra.main(config_path="conf/", config_name="config.yaml")
 def main(cfg: DictConfig) -> None:
 
-    # TODO: this is a dump from NATURAL and won't run as is. Nikita to edit to structure data and compute ATE.
-
-    dataset = instantiate(cfg.data.dataset_class)
-    llm = get_model(cfg)
+    experiment = Experiment(cfg.eval.nct_id, cfg.eval.data_path) # TODO: create Tirzepatide exp
+    sample_model = instantiate(cfg.sample.model, response_format={"type": "json_object"})
     nest_asyncio.apply()
-    llm_posts_df = pd.read_csv(cfg.data.posts_path, index_col=0).head(cfg.data.dataset_size)
-    structured_columns, Y_cols, Z_cols = dataset.XYZ_names()
-    structured_columns.extend([Y_cols, Z_cols, "post"])
 
-    llm_samples_df = pd.DataFrame(columns = structured_columns)
-    if "relevant" in cfg.data.posts_path:
-        # hard filter out posts that do not contain treatment and outcome information
-        llm_posts_df = dataset.hard_filter_ty(llm_posts_df)
-    elif "inclusion" in cfg.prompt_type:
-        # hard filter out posts that are known to not match inclusion criteria
-        llm_posts_df = dataset.discretize(llm_posts_df)
-        llm_posts_df = llm_posts_df.map(lambda x: np.nan if x in ["Unknown", "unknown"] else x)
+    curated_df = pd.read_csv(experiment.curated_data_path, index_col=0)
+    # extract samples from reports, allowing LLM to output "unknown" for missing info
+    unknowns_path = os.path.join(cfg.save_path, f"{experiment.nct_id}_{sample_model.model_name}_samples_unknowns.csv")
+    samples_with_unknown = extract_covariates(
+        curated_df, experiment, sample_model, unknowns_path
+    )
     
-    llm_inputs = []
-    save_path = save_path_fn(cfg)
+    # filter reports known to violate inclusion criteria
+    inclusion_filtered = filter_by_inclusion(samples_with_unknown, experiment)
+    # impute samples from reports, imputing missing info
+    
+    imputed_path = os.path.join(cfg.save_path, f"{experiment.nct_id}_{sample_model.model_name}_samples_imputed.csv")
+    imputed_samples = extract_covariates(
+        inclusion_filtered, experiment, sample_model, imputed_path, impute=True
+    )
+    
+    probs_model = instantial(cfg.probs.model)
 
-    if cfg.restore:
-        llm_samples_df = pd.read_csv(save_path, index_col=0) 
-        llm_posts_df = llm_posts_df.iloc[len(llm_samples_df):]
+    # extract conditionals of the form P(T, Y | X, R)
+    conditionals_path = os.path.join(cfg.save_path, f"{experiment.nct_id}_{probs_model.model_name}_conditionals.csv")
+    conditionals = extract_conditionals(imputed_samples, experiment, probs_model, conditionals_path)
+    
+    # extract inclusion probabilities of the form P(X in I | R)
+    inclusion_path = os.path.join(cfg.save_path, f"{experiment.nct_id}_{probs_model.model_name}_inclusion_probs.csv")
+    inclusion_probs = extract_conditionals(
+        imputed_samples, experiment, probs_model, inclusion_path, inclusion=True
+    )
 
-    for i, row in tqdm(llm_posts_df.iterrows()):
-        post = row["post"]
-        llm_inputs.append({"post": post})
-        if len(llm_inputs) >= cfg.model.batch_size or len(llm_inputs) == cfg.data.dataset_size:
-            llm_out_dicts = llm.get_outputs(llm_inputs)
-            llm_out_dicts = [json.loads(text) for text in llm_out_dicts]
-            dict_to_save = [{**llm_out_dicts[j], **{'post': llm_inputs[j]["post"]}} for j in range(len(llm_inputs))]
-            df_to_save = pd.DataFrame.from_dict(dict_to_save)
-            llm_samples_df = pd.concat([llm_samples_df, df_to_save], ignore_index=True)
-            if "inclusion" in cfg.prompt_type:
-                llm_posts_df.update(llm_samples_df, overwrite=False)
-                llm_samples_df = llm_posts_df.copy().iloc[:len(llm_samples_df)]
-                llm_samples_df = dataset.discretize(llm_samples_df, hard_filter=False, inf=False)
-            llm_samples_df.to_csv(save_path) 
-            llm_inputs = []
+    # TODO: compute ATE estimate
 
-
-    save_path_final = save_path_fn(cfg, final=True)
-    llm_samples_df.to_csv(save_path_final)
 
 if __name__ == "__main__":
     main()
--- naturalv2/utils.py
+++ naturalv2/utils.py
@@ -58,3 +58,64 @@ def check_trial(trial):
                 if binary:
                     return stats, True
     return stats, False
+
+def qa_interleaved_enum(q_dct, options_dct, a_enum, to_enum):
+    all_interleaved_options = []
+    alph = ["a) ", "b) ", "c) ", "d) "]
+    for option in a_enum:
+        interleaved_enum = " \n\n## Questions"
+        for num in range(len(to_enum)):
+            key = to_enum[num]
+            interleaved_enum += " \n\nQ: " + q_dct[key] 
+            interleaved_enum += " \nOptions: " 
+            for i in range(len(options_dct[key])):
+                interleaved_enum += alph[i] + options_dct[key][i] + " "
+            split_option = [i.split(":") for i in option.split(",")]
+            interleaved_enum += " \nA: " + split_option[num][1][1:]
+        all_interleaved_options.append(interleaved_enum)
+    return all_interleaved_options
+
+def concatenate_q(dct):
+    keys = list(dct.keys())
+    num = 1
+    all_qs = " \nAnswer the following questions."
+    for key in keys:
+        all_qs += " \nQ" + str(num) + ": " + dct[key]
+        num += 1
+    all_qs += "\n"
+    return all_qs
+
+def enumerate_strings(dct, string=True):
+    keys = list(dct.keys())
+    keys.reverse()
+    num = len(keys)
+    all_enumerated = dct[keys[0]]
+    all_enumerated = ["A" + str(num) + ": " + e for e in all_enumerated]
+    for key in keys[1:]:
+        num -= 1
+        cur_len = len(all_enumerated)
+        all_enumerated *= len(dct[key])
+        for j in range(len(dct[key])):
+            all_enumerated[j*cur_len : (j+1)*cur_len] = [dct[key][j] + ", " + e for e in all_enumerated[j*cur_len : (j+1)*cur_len]]
+        all_enumerated = ["A" + str(num) + ": " + e for e in all_enumerated]
+    return all_enumerated
+
+def enum_to_dcts(enumerated, to_enum):
+    return_dcts = []
+    for elem in enumerated:
+        separate = [i.split(":") for i in elem.split(",")]
+        dct = {}
+        for field in range(len(to_enum)):
+            dct["sample_" + to_enum[field]] = separate[field][1][1:]
+        return_dcts.append(dct)
+    return return_dcts
+
+def get_sample_text(dct, dataset):
+    all_keys = list(dct.keys())
+    questions = dataset.get_question_prompt(all_keys)
+    dct = dataset.interpret_samples(dct)
+    return_text = " \n\n## Questions and their correct answers"
+    for key in all_keys:
+        return_text += "\nQ: " + questions[key] + " A: " + str(dct[key]) + "."
+    return return_text
+    
