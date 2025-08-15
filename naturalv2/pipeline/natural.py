196f2d0de43b741fc1d919c5a1172402b14065b0
fcogidi <41602287+fcogidi@users.noreply.github.com>
2025-08-15 08:23:56 -0400

    Improve error logging in NATURALPipeline to include exception traceback

--- naturalv2/pipeline/natural.py
+++ naturalv2/pipeline/natural.py
@@ -256,7 +256,9 @@ class NATURALPipeline:
                         )
                         break
             except Exception as e:
-                logger.error(f"Error processing stage {stage.stage_name}: {e}")
+                logger.error(
+                    f"Error processing stage {stage.stage_name}: {e}", exc_info=True
+                )
                 raise ProcessingError from e
 
         logger.info(
