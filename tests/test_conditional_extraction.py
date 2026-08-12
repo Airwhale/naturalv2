569ba692e6276ab59ca9ba274e6c5dce0494e45e
Shaun Geer <scgeer@gmail.com>
2026-08-12 13:24:10 -0700

    Simplify detokenize helper test

--- tests/test_conditional_extraction.py
+++ tests/test_conditional_extraction.py
@@ -2,11 +2,6 @@ from naturalv2.models.lm import VLLMModel
 from naturalv2.pipeline.conditional_extraction import _detokenize_kwargs
 
 
-def test_detokenize_disabled_for_in_process_vllm():
-    llm = object.__new__(VLLMModel)
-
-    assert _detokenize_kwargs(llm) == {"detokenize": False}
-
-
-def test_detokenize_omitted_for_hosted_model():
+def test_detokenize_kwargs():
+    assert _detokenize_kwargs(object.__new__(VLLMModel)) == {"detokenize": False}
     assert _detokenize_kwargs(object()) == {}
